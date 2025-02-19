c24d0bf14f1108eb0f69c31a313b6883b086fef8
U-BSD\419236 <419236@SHS-8YBF6X3.beaverton.k12.or.us>
2025-02-19 11:47:01 -0800

    layout set up

---
+++ heap.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void print();
+void insert();
+void remove();
+void removeAll();
+
+int main() {
+  int heap[100];
+  
+  return 0;
+}
+
+void print() { //visual printing of array
+
+}
+
+void insert() { //insert index at empty rightmost spot
+
+}
+
+void remove() { //remove root, print it, then resort table
+
+}
+
+void removeAll() { //call remove until heap is empty
+
+}
