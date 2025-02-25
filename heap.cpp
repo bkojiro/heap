29128c23101495cf0e7447fbc745f45de3faf0e5
U-BSD\419236 <419236@SHS-8YBF6X3.beaverton.k12.or.us>
2025-02-25 11:46:55 -0800

    final save

--- heap.cpp
+++ heap.cpp
@@ -5,10 +5,11 @@
 
 using namespace std;
 
-void print(int* heap);
-void insert(int x, int* &heap);
-void remove();
-void removeAll();
+int size(int* heap);
+void print(int* heap, int pos, int depth, int size);
+void insert(int* &heap, int x, int index);
+void resort(int* &heap, int index);
+void removeAll(int* &heap, int size);
 
 int main() {
   int* heap = new int[101];
@@ -19,7 +20,7 @@ int main() {
     cin.get(action, 11);
     cin.ignore();
     if (strcmp(action, "PRINT") == 0) { //print table
-      print(heap);
+      print(heap, 1, 0, size(heap));
     } else if (strcmp(action, "INSERT") == 0) { //insert at next open slot
       cout << "Type 'f' to insert by file or 'n' to insert by number" << endl << "> ";
       char fileornumber;
@@ -28,11 +29,13 @@ int main() {
       if (fileornumber == 'f') {
 	//grab number file
 	ifstream numbers;
-	numbers.open("newNums.txt");
+	numbers.open("numbers.txt");
 	int x;
 	while (numbers >> x) {
 	  //insert x into heap
-	  insert(x, heap);
+	  int index = size(heap);
+	  heap[index] = x;
+	  insert(heap, x, index);
 	}
 	numbers.close();
       } else if (fileornumber == 'n') {
@@ -41,14 +44,19 @@ int main() {
 	int x;
 	cin >> x;
 	cin.ignore();
-	insert(x, heap);
+	int index = size(heap);
+	heap[index] = x;
+	insert(heap, x, index);
       } else {
 	cout << "Invalid input" << endl;
       }
     } else if (strcmp(action, "REMOVE") == 0) { //remove root and print, then move everything accordingly
-      
+      cout << "Root: " << heap[1] << endl;
+      heap[1] = heap[size(heap) - 1];
+      heap[size(heap) - 1] = 0;
+      resort(heap, 1);
     } else if (strcmp(action, "REMOVEALL") == 0) { //continuously remove and print
-      
+      removeAll(heap, size(heap));
     } else if (strcmp(action, "QUIT") == 0) { //quit program
       cout << "Program terminated";
       running = false;
@@ -59,43 +67,64 @@ int main() {
   return 0;
 }
 
-void print(int* heap) { //visual printing of array
+int size(int* heap) {
   int index = 1;
-  while (heap[index + 1] != 0) {
+  while(heap[index] != 0) {
     index++;
   }
-  int levels = ceil(log2(index)) + 1;
-  //reset index once # of levels is found
-  index = 1;
-  while (heap[index] != 0) {
-    //find out how to visualize
+  return index;
+}
+
+void print(int* heap, int pos, int depth, int size) { //visual printing of array
+  if (pos * 2 + 1 < size) { //check left NOT null
+    print(heap, pos * 2 + 1, depth + 1, size);
+  }
+  for (int a = 0; a < depth; a++) {
+    cout << '\t';
+  }
+  cout << heap[pos] << endl;
+  if (pos * 2 < size) {
+    print(heap, pos * 2, depth + 1, size);
   }
 }
 
-void insert(int x, int* &heap) { //insert index at empty rightmost spot
-  //NOTE: right child is index * 2 and left child is index * 2 + 1 so next open slot in array is rightmost position
-  int index = 1;
-  while (heap[index] != 0) {
-    index++; //next available slot in array
-  }
-  heap[index] = x;
+void insert(int* &heap, int x, int index) {
   int parentID = floor(index/2);
   int parent = heap[parentID];
-  while (x > parent && index != 1) {
+  if (x > parent && index != 1) {
     int temp = index;
-    index = parentID; //swap index
-    heap[index] = x; //insert x at parent index
+    index = parentID;
+    heap[index] = x;
     heap[temp] = parent;
-    //now reevaluate parent
-    parentID = floor(index/2);
-    parent = heap[parentID];
+    insert(heap, x, index);
   }
 }
 
-void remove() { //remove root, print it, then resort table
-
+void resort(int* &heap, int index) { //recursively go through heap and make children smaller than parent
+  int rightID = index * 2;
+  int leftID = rightID + 1;
+  if (heap[index] < heap[leftID]) { //parent is smaller than left child
+    int temp = heap[index];
+    heap[index] = heap[leftID];
+    heap[leftID] = temp;
+    
+    resort(heap, leftID);
+  }
+  if (heap[index] < heap[rightID]) {
+    int temp = heap[index];
+    heap[index] = heap[rightID];
+    heap[rightID] = temp;
+    
+    resort(heap, rightID);
+  }
 }
 
-void removeAll() { //call remove until heap is empty
-
+void removeAll(int* &heap, int size) { //call remove until heap is empty
+  cout << "Removing values: ";
+  for (int a = 1; a < size; a++) {
+    cout << heap[1] << "  ";
+    heap[1] = heap[size - a];
+    heap[size - a] = 0;
+    resort(heap, 1);
+  }
 }
